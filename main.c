6c5ecab0d6d42fd16fc52856bb9b120a60b7c3a3
Karthik Gooli <grkrthk@yahoo.co.in>
2014-10-21 01:27:01 -0500

    minor changes

--- main.c
+++ main.c
@@ -3,13 +3,9 @@
 #include"file_cache_def.h"
 #include"file_cache_decl.h"
 
-extern struct free_pool_data* free_pool_table;
-extern struct file_cache* file_cache_head;
 
 void main(){
         struct file_cache* my_inst = file_cache_construct(10);
         struct file_cache* next_inst = file_cache_construct(20);
-        int stat = initialize_free_pool_data(1000);
-        //file_cache_pin_files();
-
+        int stat = initialize_free_pool_data(1000);        
 }
